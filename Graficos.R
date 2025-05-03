419aadf8e0ba11b024adab97a67cccb94c395918
Osvaldo <pagioliosvaldo@gmail.com>
2025-05-02 23:08:29 -0300

    grafico de soluções

--- Graficos.R
+++ Graficos.R
@@ -2,11 +2,12 @@ library(ggplot2)
 
 tExec <- read.csv("resultados.csv")
 
-ggplot(data = tExec, aes(x = Quantidade.trabalhos))+
+ggplot(data = tExec, aes(x = Quantidade))+
   #geom_line(aes(y = exponencial, color = "Exponencial O(k^n)"))+
   #geom_line(aes(y = aproximado))+
-  geom_smooth(aes(y = exponencial), method = "loess", se = FALSE, color = "red") +
-  scale_y_time(name = "Tempo de execução (s)")+
+  geom_smooth(aes(y = Solução.aproximado), method = "loess", se = FALSE, color = "blue")+
+  geom_smooth(aes(y = Solução.exponencial), method = "loess", se = FALSE, color = "red") +
+  scale_y_continuous(name = "Quantidade de caixas usadas")+
   scale_x_continuous(name = "Quantidade de itens")+
   #labs(colour = "Algoritmos") +
   theme_bw()
